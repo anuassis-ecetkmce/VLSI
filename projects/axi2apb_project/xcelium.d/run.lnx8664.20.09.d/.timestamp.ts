a94e659008f1b2945590792c5f955eb79f9440a9
Hrh06 <haneenharis100@gmail.com>
2026-01-10 15:06:30 +0530

    git ignore file is added and ignored run files

--- projects/axi2apb_project/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
+++
@@ -1,2 +0,0 @@
-1753689648 /home/student/AXI_protocol_verification/VLSI/projects/axi2apb_project/design/seq_design.v
-1753689762 /home/student/AXI_protocol_verification/VLSI/projects/axi2apb_project/testbench/seq_tb.v
