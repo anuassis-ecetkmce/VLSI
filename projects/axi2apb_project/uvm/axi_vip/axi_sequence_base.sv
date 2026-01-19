20c1f744c425e72b1047a8afd2a566ec078af695
Ayisha <ayishanoushad234@gmail.com>
2026-01-20 01:39:02 +0530

    Updated axi_sequence_base implementation

--- projects/axi2apb_project/uvm/axi_vip/axi_sequence_base.sv
+++ projects/axi2apb_project/uvm/axi_vip/axi_sequence_base.sv
@@ -4,7 +4,6 @@
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 import axi_types_pkg::*;
-`include "axi_item_base.sv"
 
 /**
  * Base class for all AXI sequences
@@ -12,7 +11,6 @@ import axi_types_pkg::*;
  * - Can be extended by other sequences if needed
  */
 class axi_sequence_base extends uvm_sequence #(axi_transaction);
-
 
   `uvm_object_utils(axi_sequence_base)
 
@@ -24,20 +22,19 @@ class axi_sequence_base extends uvm_sequence #(axi_transaction);
   // -------------------------------------------------
   // Helper task to create a generic AXI transaction
   // -------------------------------------------------
-  virtual task create_transaction(ref axi_item_base tr,
-                                  bit is_write = 0,
+  virtual task create_transaction(bit is_write = 0,
                                   int unsigned addr = 0,
                                   int unsigned len = 0,
                                   axi_size_t size = AXI_SIZE_4B,
                                   axi_burst_t burst = AXI_BURST_INCR);
-    tr = axi_item_base::type_id::create("axi_tr", this);
+    axi_transaction tr;
+    tr = axi_transaction::type_id::create("axi_tr");
     tr.is_write = is_write;
     tr.addr     = addr;
     tr.len      = len;
     tr.size     = size;
     tr.burst    = burst;
     tr.resp     = AXI_RESP_OKAY;
-    tr.err      = 0;
   endtask
 
   // -------------------------------------------------
